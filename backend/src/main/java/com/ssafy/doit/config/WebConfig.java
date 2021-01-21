bf0810dc846d1d194163ff397025218440c74704
skyyy6 <skyyy6@naver.com>
2021-01-21 22:06:19 +0900

    backend init

---
+++ backend/src/main/java/com/ssafy/doit/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ssafy.doit.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class WebConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/account/*").permitAll();
+    }
+}
---
+++ backend/src/main/java/com/ssafy/doit/domain/UserRole.java
@@ -0,0 +1,14 @@
+package com.ssafy.doit.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+    ADMIN("ROLE_ADMIN", "관리자"),
+    USER("ROLE_USER", "사용자");
+
+    private final String nkey;
+    private final String title;
+}
