0042686bb49f34bf53922b1604b6e24a56542757
kimbuhui <kimbuhui@naver.com>
2021-01-22 14:38:28 +0900

    fix : User related

--- backend/src/main/java/com/ssafy/doit/model/Group.java
+++ backend/src/main/java/com/ssafy/doit/model/Group.java
@@ -3,6 +3,7 @@ package com.ssafy.doit.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
@@ -16,13 +17,26 @@ public class Group {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long groupPk;
+    @Column(name = "group_pk")
+    private Long id;
 
     @JsonIgnore
     private String name;    // 그룹 명
     private String content; // 그룹 소개글
-    private Integer score;  // 그룹 점수
-    private Integer total;  // 총 인원
-    private Integer max;    // 최대 인원
-    private Integer leader; // 팀장
+    private int max;    // 최대 인원
+    private int leader; // 팀장
+
+    @ColumnDefault("0")
+    private int score;  // 그룹 점수
+
+    @ColumnDefault("1")
+    private int total;  // 총 인원
+
+    @Builder
+    public Group(String name, String content, int max, int leader ){
+        this.name = name;
+        this.content = content;
+        this.max = max;
+        this.leader = leader;
+    }
 }
