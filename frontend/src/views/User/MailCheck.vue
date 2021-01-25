10aae7fc0aa859a9235584c43fe3ee525bf0febf
skyyy6 <skyyy6@naver.com>
2021-01-25 22:02:13 +0900

    Feat: add Update_UI

--- frontend/src/views/User/MailCheck.vue
+++ frontend/src/views/User/MailCheck.vue
@@ -10,27 +10,32 @@
             max-width="60"
           />
           
-          <div class="my-3 text-h3">인증 메일이 발송되었습니다</div>
+          <h1 class="my-3 text-center">인증 메일이 발송되었습니다</h1>
           <div class="my-3">
-            <div class="subtitle-1">
-              메일함에서(<a href='#'>{{link}}</a>) 인증 메일을 확인 바랍니다.
+            <div class="text-center">
+              <subtitle-1>메일함에서(<a href='#'>{{link}}</a>) 인증 메일을 확인 바랍니다.</subtitle-1>
             </div>
-            <div class="subtitle-1">
-              이메일의 인증 버튼을 선택하면 회원 가입이 완료됩니다.
+            <div class="text-center">
+              <subtitle-1>이메일의 인증 버튼을 선택하면 회원 가입이 완료됩니다.</subtitle-1>
             </div>
           </div>
 
           <div class="pa-10 my-3 sub-box">
-            <div class="h5 mb-3 text-center">유의사항</div>
-            <div class="subtitle-1 my-3">
-                1. 인증 메일은 발송 시점으로부터 24시간 동안만 유효하며, 재발송 시
-                기존 인증 코드는 만료됩니다. 반드시 마지막에 수신된 메일을 확인 바랍니다.
+            <h4 class="mb-3 text-center">유의사항</h4>
+            <div class="my-3">
+                <subtitle-1>
+                    1. 인증 메일은 발송 시점으로부터 24시간 동안만 유효하며, 재발송 시
+                    기존 인증 코드는 만료됩니다. 반드시 마지막에 수신된 메일을 확인 바랍니다.
+                </subtitle-1>
             </div>
-            <div class="subtitle-1 my-3">
-                2. 메일이 도착하지 않았다면 스팸함을 확인해 주시기 바랍니다.
+            <div class="my-3">
+                <subtitle-1>
+                    2. 메일이 도착하지 않았다면 스팸함을 확인해 주시기 바랍니다.
+                </subtitle-1>
             </div>
-            <div @click="resend" class="pt-3 text-center">
+            <div class="pt-3 text-center">
                 <v-btn 
+                    @click="resend"
                     color=#F9802D
                     outlined
                     rounded
--- frontend/src/views/User/Update.vue
+++ frontend/src/views/User/Update.vue
@@ -1,8 +1,70 @@
 <template>
   <div>
     <Header></Header>
-    <v-container class="pa-8 pa-sm-16 d-flex align-center flex-column mx-auto">
-        
+    <v-container class="pa-8 pa-sm-16">
+        <form>
+            <v-col cols="12">
+                <v-row justify="center">
+                    <v-col cols="12" md="3" lg="2">
+                        <div class="text-center">
+                            <img
+                                src="@/assets/logo/profile_temp.png"
+                                alt="profile-img"
+                                class="profile-img"
+                            />
+                            
+                            <div class="pt-3 text-center">
+                                <v-btn 
+                                    @click="changhPhoto"
+                                    outlined
+                                    rounded
+                                >사진 변경</v-btn>
+                            </div>
+                        </div>
+                    </v-col>
+
+                    <v-col cols="12" md="5" lg="3">
+                        <v-row no-gutters class="d-flex flex-nowrap m" >
+                            <v-subheader>닉네임</v-subheader>
+                            <v-text-field
+                                v-model="name"
+                                required
+                                clearable
+                                :error-messages="nameErrors"
+                                :counter="8"
+                                @input="$v.name.$touch()"
+                                @blur="$v.name.$touch()"
+                            ></v-text-field>
+                        </v-row>
+                        <v-row no-gutters class="d-flex flex-nowrap m" >
+                            <v-subheader>이메일</v-subheader>
+                            <v-text-field
+                                v-model="email"
+                                required
+                                disabled
+                                @input="$v.name.$touch()"
+                                @blur="$v.name.$touch()"
+                            ></v-text-field>
+                        </v-row>
+                        <v-row no-gutters class="d-flex flex-nowrap m" >
+                            <v-subheader>비밀번호</v-subheader>
+                            <div class="pt-3 text-center">
+                                <v-btn 
+                                    @click="changePW"
+                                    outlined
+                                    rounded
+                                >비밀번호 변경</v-btn>
+                            </div>
+                        </v-row>
+                    </v-col>
+                </v-row>
+                <v-row justify="center">
+                    <v-col cols="12" md="9" lg="6" class="text-right">
+                        <v-btn text x-large>변경사항 저장 ></v-btn>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </form>
     </v-container>
     <Footer></Footer>
   </div>
@@ -16,6 +78,20 @@ export default {
     components :{
         Header,
         Footer
+    },
+    data() {
+        return {
+            name : "test",
+            email : "test@test.com"
+        }
+    },
+    methods: {
+        changhPhoto(){
+
+        },
+        changePW(){
+
+        }
     }
 }
 </script>
