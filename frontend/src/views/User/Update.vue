9783066fe6567a21083894f5dd3393f51033ee1d
skyyy6 <skyyy6@naver.com>
2021-01-25 13:30:30 +0900

    Modify: modify Mailcheck_UI

---
+++ frontend/src/views/User/Update.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header></Header>
+    <v-container class="pa-8 pa-sm-16 d-flex align-center flex-column mx-auto">
+        
+    </v-container>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/common/Header.vue";
+import Footer from "@/components/common/Footer.vue";
+export default {
+    name : "Update",
+    components :{
+        Header,
+        Footer
+    }
+}
+</script>
+
+<style>
+
+</style>
