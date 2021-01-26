87feb4cd82f8d3b0203d661f2cc9d7ffaf4f4e7c
nate199458 <nate199458@gmail.com>
2021-01-26 23:00:07 +0900

    add Complete.vue

---
+++ frontend/src/views/User/Complete.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <Header></Header>
+      <v-container class="pa-8 pa-sm-16 d-flex align-center flex-column mx-auto">
+        <h3>가입 완료</h3>
+      </v-container>
+  <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/common/Header.vue";
+import Footer from "@/components/common/Footer.vue";
+
+export default {
+  components: {Header, Footer},
+
+}
+</script>
+
+<style>
+
+</style>
