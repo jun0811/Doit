d80cad463b145fadd6ef07a0f1df45f16c0b7ffd
kimbuhui <kimbuhui@naver.com>
2021-01-26 10:52:34 +0900

    Feat: GroupListByHashTag JoinedGroup

--- backend/src/main/java/com/ssafy/doit/model/HashTag.java
+++ backend/src/main/java/com/ssafy/doit/model/HashTag.java
@@ -1,9 +1,6 @@
 package com.ssafy.doit.model;
 
 import lombok.*;
-import org.hibernate.annotations.ColumnDefault;
-import org.springframework.stereotype.Service;
-
 import javax.persistence.*;
 
 @Entity
---
+++ backend/src/main/java/com/ssafy/doit/service/GroupUserService.java
@@ -0,0 +1,42 @@
+package com.ssafy.doit.service;
+
+import com.ssafy.doit.model.Group;
+import com.ssafy.doit.model.GroupUser;
+import com.ssafy.doit.model.user.User;
+import com.ssafy.doit.repository.GroupRepository;
+import com.ssafy.doit.repository.GroupUserRepository;
+import com.ssafy.doit.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GroupUserService {
+
+    @Autowired
+    private GroupUserRepository groupUserRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Transactional
+    // public void join(Authentication authentication, Group request)
+    public void join(Long groupPk) {
+        //User user = (User) authentication.getPrincipal();
+        User user = userRepository.findByEmail("buhee1029@gmail.com").get(); // 테스트용
+        Group group = groupRepository.findById(groupPk).get();
+
+        groupUserRepository.save(GroupUser.builder()
+                .group(group)
+                .user(user)
+                .build());
+    }
+
+    // 가입한 그룹 가져오기
+}
