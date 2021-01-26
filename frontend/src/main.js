71003192d647cfaf67efa26c8fe63b03153a6008
skyyy6 <skyyy6@naver.com>
2021-01-26 10:09:58 +0900

    Fix: fix main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router';
-import routes from './routes'
-import store from './store/index'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify';
+
 
 Vue.config.productionTip = false;
-
-Vue.use(VueRouter);
-
-export const router = new VueRouter({
-    routes,
-});
 
 new Vue({
     router,
     store,
+    vuetify,
     render: h => h(App),
     created() {
         const accessToken = sessionStorage.getItem("accessToken");
