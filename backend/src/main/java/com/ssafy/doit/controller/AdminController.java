21935c375744414a4f10bf1fa9c9c293e4747f32
한지현 <gksgpals96@naver.com>
2021-02-01 21:57:02 +0900

    Feat:관리자 전체 그룹 리스트 확인

--- backend/src/main/java/com/ssafy/doit/controller/AdminController.java
+++ backend/src/main/java/com/ssafy/doit/controller/AdminController.java
@@ -1,10 +1,12 @@
 package com.ssafy.doit.controller;
 
+import com.ssafy.doit.model.Group;
 import com.ssafy.doit.model.response.ResponseBasic;
 import com.ssafy.doit.model.response.ResponseGroup;
 import com.ssafy.doit.model.user.User;
 import com.ssafy.doit.model.user.UserRole;
 import com.ssafy.doit.repository.AdminRepository;
+import com.ssafy.doit.repository.GroupRepository;
 import com.ssafy.doit.repository.UserRepository;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -22,11 +24,13 @@ import java.util.Optional;
 public class AdminController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private GroupRepository groupRepository;
 
-    //회원 리스트
-    @ApiOperation(value = "회원 리스트")
-    @GetMapping("/searchUser")
-    public Object searchGroup(){ // 페이징 처리하기
+    //관리자 - 회원 리스트
+    @ApiOperation(value = "관리자 - 회원 리스트")
+    @GetMapping("/searchAllUser")
+    public Object searchAllUser(){ // 페이징 처리하기
         ResponseBasic result = new ResponseBasic();
         List<User> list = userRepository.findAll();
 
@@ -40,10 +44,10 @@ public class AdminController {
         }
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
-    //회원 삭제
+    //관리자 - 회원 삭제
     @ApiOperation(value = "관리자 - 회원 탈퇴")
     @PutMapping("/beDeletedUser")
-    public Object deleteUser(@RequestParam Long id) {
+    public Object beDeletedUser(@RequestParam Long id) {
         ResponseBasic result = new ResponseBasic();
         try {
             Optional<User> userInfo = userRepository.findById(id);
@@ -63,4 +67,27 @@ public class AdminController {
         }
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
+
+
+    //관리자 - 그룹 리스트
+    @ApiOperation(value = "관리자 -그룹 리스트")
+    @GetMapping("/searchAllGroup")
+    public Object searchAllGroup(){ // 페이징 처리하기
+        List<Group> list = groupRepository.findAll();
+        ResponseBasic result = new ResponseBasic();
+        if(list.size() == 0){
+            result.status =false;
+            result.data= "fail";
+        }else{
+            result.status = true;
+            result.data = "success";
+            result.object = list;
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    //그룹 삭제
+//    froup has  어짜고 테이블@@@ 삭제 잘 보고 0--
+
+    //피드 삭제
 }
