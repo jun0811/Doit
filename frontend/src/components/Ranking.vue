d1690be085a1df4060ae27b948577ed8ca394b19
dryans <dryans@naver.com>
2021-02-02 16:58:10 +0900

    Modify : Main Ranking border

--- frontend/src/components/Ranking.vue
+++ frontend/src/components/Ranking.vue
@@ -4,15 +4,16 @@
       Doit Ranking👑
     </v-row>
     <v-row class="py-10">
-      <v-col class="mx-3 mx-sm-16 temp-box d-flex">
-        <!-- Ranking👑 -->
-        <div v-for="(group, idx) in groups" :key="idx" class="rank-box d-flex align-start" >
-          <div style="height:40px; width:40px;">
-            <h1 style="height:40px">{{idx+1}}</h1>
+      <v-col class="mx-3 mx-sm-16 temp-box d-flex justify-space-around">
+        <div v-for="(group, idx) in groups" :key="idx" class="rank-box d-flex" >
+          <div class="">
+            <h1 class="rank-num">{{idx+1}}</h1>
           </div>
-          <div class="d-flex flex-column">
-            <img class="rank-image" :src="group.groupImage" alt="그룹이미지">
-            <p style="height:50px; text-align:center; display:inline">{{group.groupname}}</p>
+          <div class="d-flex flex-column justify-space-around">
+            <div class="rank-wrapper">
+              <img class="rank-image box" :src="group.groupImage" alt="그룹이미지">
+            </div>
+            <div class="group-name">{{group.groupname}}</div>
           </div>
         </div>
       </v-col>
@@ -52,17 +53,80 @@ export default {
 
 .rank-box {
   width: 150px;
-  height: 200px;
+  height: 100%;
 }
 
+.rank-num {
+  height:40px;
+  width:40px;
+}
+
+  
+.group-name {
+  height:30px; 
+  text-align:center;
+}
+.rank-wrapper {
+  border-radius: 50%;
+}
 
 
 .rank-image {
   width: 100px;
   height: 100px;
   border-radius: 50%;
-  border: 3px solid orange;
-  display: inline;
 }
+
+/* --- */
+.box {
+  position: relative;
+  width: 100px;
+  height: 100px;
+  border: 5px solid orange;
+  display: block;
+  border-color: orange;
+  animation-name: flash_border;
+  animation-duration: 2s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  -webkit-animation-name: flash_border;
+  -webkit-animation-duration: 2s;
+  -webkit-animation-timing-function: linear;
+  -webkit-animation-iteration-count: infinite;
+  -moz-animation-name: flash_border;
+  -moz-animation-duration: 2s;
+  -moz-animation-timing-function: linear;
+  -moz-animation-iteration-count: infinite;
+}
+
+@keyframes flash_border {
+  0% {
+    border-color: #5f86f2;
+  }
+  14% {
+    border-color:#a65ff2;
+  }
+  28% {
+    border-color: #f25fd0;
+  }
+  42% {
+    border-color: #f25f61;
+  }
+  56% {
+    border-color: #f2cb5f;
+  }
+  70% {
+    border-color:#abf25f;
+  }
+  84% {
+    border-color:#5ff281,;
+  }
+  100% {
+    border-color:#5ff2f0;
+  }
+}
+
+
+
 
 </style>
