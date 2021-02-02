334eb36d3c8dd43eb3506c7f8a6e460632c6072f
김부희 <kimbuhui@naver.com>
2021-02-02 16:36:00 +0900

    Feat: 그룹내피드생성, 그룹내피드리스트

--- backend/src/main/java/com/ssafy/doit/model/Feed.java
+++ backend/src/main/java/com/ssafy/doit/model/Feed.java
@@ -1,38 +1,51 @@
 package com.ssafy.doit.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.*;
 import javax.persistence.Entity;
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name= "`feed`")
 public class Feed {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long feedPk;
 
-    private Long groupPk;
-    private String userPk;
     private String media;
-
-    @Column(columnDefinition = "LONGTEXT", nullable = false)
     private String content;
+    private String feedType; // 인증피드 : true, 자유피드 : false
 
     private int authCnt;
-
-    @CreatedDate
-    private Date authDate;
-
+    private String authDate;
     private String authCheck;
 
-    private String feedType;
+    private LocalDateTime createDate;
+    private String updateDate;
+
     private String status;
+    private Long groupPk;
+    private Long userPk; // writer
+
+    @Builder
+    public Feed(String content, String feedType, LocalDateTime createDate,
+                Long groupPk, Long userPk){
+        //this.media = media;
+        this.content = content;
+        this.feedType = feedType;
+        this.groupPk = groupPk;
+        this.userPk = userPk;
+        this.createDate = createDate;
+
+        this.updateDate = "0-0-0 0:0:0";
+        this.authDate = "0-0-0 0:0:0";
+        this.authCheck = "false";
+        this.status = "true";
+    }
 
 }
