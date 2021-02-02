7ef87447d6cc89dc47f546d5132d9c1d9847008b
최예은 <skyyy6@naver.com>
2021-02-02 15:26:06 +0900

    Feat: add createProduct

--- backend/src/main/java/com/ssafy/doit/controller/ProductController.java
+++ backend/src/main/java/com/ssafy/doit/controller/ProductController.java
@@ -1,5 +1,42 @@
 package com.ssafy.doit.controller;
 
-public class ProductController {
+import com.ssafy.doit.model.Product;
+import com.ssafy.doit.model.response.ResponseBasic;
+import com.ssafy.doit.repository.ProductRepository;
+import com.ssafy.doit.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @ApiOperation(value = "물품 등록")
+    @PostMapping("/createProduct")
+    public Object createProduct(@RequestBody Product product){
+        ResponseBasic result = null;
+        try{
+            long currentUser = userService.currentUser();
+            product.setUserPk(currentUser);
+            productRepository.save(product);
+            result = new ResponseBasic(true, "success", null);
+        }catch (Exception e){
+            result = new ResponseBasic(false, "fail", null);
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = )
 }
--- backend/src/main/java/com/ssafy/doit/repository/ProductRepository.java
+++ backend/src/main/java/com/ssafy/doit/repository/ProductRepository.java
@@ -1,8 +1,13 @@
 package com.ssafy.doit.repository;
 
 import com.ssafy.doit.model.Product;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    List<Product> findByTitleLike(String title);
 }
