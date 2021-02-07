83bee84f2d27f3542b2e24bd298510dc54d99aee
김소정 <itoggi0328@naver.com>
2021-02-07 17:01:32 +0900

    fix: 메인페이지 아이콘 hover효과 추가

--- frontend/src/components/Category.vue
+++ frontend/src/components/Category.vue
@@ -5,31 +5,31 @@
     </v-row>
     <v-card class="mx-auto mx-3 mx-sm-16">
       <v-row class="py-8 px-5 d-flex justify-space-around">
-        <v-col cols="4" sm="1" class="d-flex flex-column align-center">
+        <v-col cols="4" sm="1" class="d-flex flex-column align-center category-icon-effect">
           <img src="@/assets/icon/study.png" alt="study-icon" class="category-icon">
           <div class="group-name mt-2">공부</div>
         </v-col>
-        <v-col cols="4" sm="1" class="d-flex flex-column align-center">
+        <v-col cols="4" sm="1" class="d-flex flex-column align-center category-icon-effect">
           <img src="@/assets/icon/diet.png" alt="diet-icon" class="category-icon">
           <div class="group-name mt-2">다이어트</div>
         </v-col>
-        <v-col cols="4" sm="1" class="d-flex flex-column align-center">
+        <v-col cols="4" sm="1" class="d-flex flex-column align-center category-icon-effect">
           <img src="@/assets/icon/exercise.png" alt="exercise-icon" class="category-icon">
           <div class="group-name mt-2">운동</div>
         </v-col>
-        <v-col cols="3" sm="1" class="d-flex flex-column align-center">
+        <v-col cols="3" sm="1" class="d-flex flex-column align-center category-icon-effect">
           <img src="@/assets/icon/book.png" alt="book-icon" class="category-icon">
           <div class="group-name mt-2">책</div>
         </v-col>
-        <v-col cols="3" sm="1" class="d-flex flex-column align-center">
+        <v-col cols="3" sm="1" class="d-flex flex-column align-center category-icon-effect">
           <img src="@/assets/icon/life.png" alt="life-icon" class="category-icon">
           <div class="group-name mt-2">생활습관</div>
         </v-col>
-        <v-col cols="3" sm="1" class="d-flex flex-column align-center">
+        <v-col cols="3" sm="1" class="d-flex flex-column align-center category-icon-effect">
           <img src="@/assets/icon/hobby.png" alt="hobby-icon" class="category-icon">
           <div class="group-name mt-2">취미</div>
         </v-col>
-        <v-col cols="3" sm="1" class="d-flex flex-column align-center">
+        <v-col cols="3" sm="1" class="d-flex flex-column align-center category-icon-effect">
           <img src="@/assets/icon/more.png" alt="etc-icon" class="category-icon">
           <div class="group-name mt-2">그 외</div>
         </v-col>
@@ -51,6 +51,7 @@ export default {
   display: inline-block;
   width: 50px;
   height: 50px;
+  cursor: pointer;
 }
 
 @media only screen and (min-width: 300px) and (max-width: 599px) {
@@ -59,6 +60,7 @@ export default {
     display: inline-block;
     width: 30px;
     height: 30px;
+    cursor: pointer;
   }
 }
 
@@ -74,4 +76,23 @@ export default {
   }
 }
 
+.category-icon-effect img{
+    -webkit-transform:scale(1);
+    -moz-transform:scale(1);
+    -ms-transform:scale(1); 
+    -o-transform:scale(1);  
+    transform:scale(1);
+    -webkit-transition:.3s;
+    -moz-transition:.3s;
+    -ms-transition:.3s;
+    -o-transition:.3s;
+    transition:.2s;
+}
+.category-icon-effect:hover img {
+    -webkit-transform:scale(1.2);
+    -moz-transform:scale(1.2);
+    -ms-transform:scale(1.2);   
+    -o-transform:scale(1.2);
+    transform:scale(1.2);
+}
 </style>
