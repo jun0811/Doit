4335cfdf30c3fa1ed8ba6f30869e614c50c5bd90
dryans <dryans@naver.com>
2021-02-07 14:41:19 +0900

    Modify : 마일리지샵 글쓰기 수정

--- frontend/src/views/Mileage/ProductWrite.vue
+++ frontend/src/views/Mileage/ProductWrite.vue
@@ -8,10 +8,10 @@
         <hr style="width:570px;" class="my-4">
         <v-row class="d-flex flex-column content-wrapper mt-1">
           <v-text-field
-            v-model="title"
             solo
             :label="title"
             clearable
+            v-model="product.title"
           ></v-text-field>
         </v-row>
         <v-card
@@ -29,6 +29,7 @@
                 :items="categories"
                 label="카테고리 선택"
                 solo
+                v-model="product.category"
               ></v-select>
             </v-col>
           </v-row>
@@ -40,7 +41,7 @@
                 :label="mileage"
                 solo
               ></v-text-field>
-              <span class="pl-2">마일리지</span>
+              <span class="ml-2">마일리지</span>
             </v-col>
           </v-row>
           <v-row class="my-2">
@@ -50,6 +51,7 @@
               name="input-7-4"
               label="Solo textarea"
               hide-details=""
+              v-model="product.content"
             ></v-textarea>
           </v-row>
           <v-row class="my-2">
@@ -59,13 +61,14 @@
               small-chips
               truncate-length="15"
               hide-details=""
+              v-on:change="product.image"
             ></v-file-input>
           </v-row>
-
         </v-card>
 
-        <v-btn class="deal-btn" 
+        <v-btn class="submit-btn" 
           large
+          @click="write"
           >
           등록하기
         </v-btn>
@@ -79,7 +82,7 @@
 import "@/assets/css/profile.css";
 import Header from "@/components/common/Header.vue";
 import Footer from "@/components/common/Footer.vue";
-import http from "../../http-common";
+import { createProduct } from "@/api/mileage/index.js"
 
 export default {
   name: "ProductWrite",
@@ -91,20 +94,40 @@ export default {
     return {
       title : "상품명 및 제목을 입력하세요.",
       categories : ['카테고리1', '카테고리2', '카테고리3', '카테고리4', '카테고리5', ],
-      mileage: 300,
+      mileage: "300",
+      product : {
+        category : '',
+        content : '',
+        image : '',
+        title : '',
+        userPk : 0,
+      },
     }
   },
-  props:{
-    product_id: Number,
-  },
-  created() {
-    http.get(`/product/${this.product_id}`)
-    .then((res)=>{
-      console.log(res.data.object)
-      this.product = res.data.object
-    })
-  },
-  computed: {
+  methods: {
+    write() {
+      createProduct(
+        {
+          "category": this.product.category,
+          "content":  this.product.content,
+          "id": 0,
+          "image": this.product.image,
+          "title":  this.product.title,
+          "userPk": this.$store.state.userpk,
+        },
+        (res) =>{
+          if (res.status){
+          alert("물품을 등록했습니다.")
+          console.log(res)
+          this.$router.push('/') // 어디로 보낼지 정하고 변경!
+          }
+        },
+        (err) =>{
+          console.log(err)
+          alert("등록 실패")
+        }
+      )
+    },
   }
 };
 </script>
@@ -124,7 +147,7 @@ export default {
   padding : 30px;
 }
 
-.deal-btn {
+.submit-btn {
   margin:30px;
   border: 2px solid orange;
   border-radius: 15px;
