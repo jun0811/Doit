d4ecb17719e4f7f076cff5d927d50a307a8e4d7e
김소정 <itoggi0328@naver.com>
2021-02-09 12:32:35 +0900

    fix: 메인페이지 UI 레이아웃 배치 완료 메인페이지 레이아웃 구성 완료(애니메이션 적용 전)

--- frontend/src/components/common/NavBar.vue
+++ frontend/src/components/common/NavBar.vue
@@ -1,35 +1,54 @@
 <template>
-  <v-container fluid class="navbar-style" fixed-header>
+  <v-container fluid fixed-header class="nav-style">
     <v-row class="d-flex justify-center">
       <v-col cols="6" sm="2" class="d-flex justify-center">
-        <v-btn text>
+        <v-btn text large>
           Doit을 소개합니다
         </v-btn>
       </v-col>
       <v-col cols="4" sm="2" class="d-flex justify-center">
-        <v-btn text>
+        <v-btn text large>
           그룹 둘러보기
         </v-btn>
       </v-col>
       <v-col cols="2" sm="2" class="d-flex justify-center">
-        <v-btn text>
+        <v-btn 
+          text 
+          large
+          @click="goToShop"
+        >
           Shop
         </v-btn>
       </v-col>
+    </v-row>
+    <v-row>
+      <div class="navbar-line" role="presentation"></div>
     </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-
+  name: "NavBar",
+  methods: {
+    goToShop() {
+      this.$router.push("/mileageshop")
+    }
+  }
 }
 </script>
 
 <style>
-.navbar-style {
-  /* background-color: #2F3857; */
-  /* color: white; */
-  border-bottom: thin solid grey;
+.navbar-line {
+  width: 100%;
+  border-bottom: 1px solid black;
+  opacity: 0.2;
+}
+
+.nav-style {
+  /* position: fixed; */
+  background-color: white;
+  margin-bottom: 50px;
+  z-index: 1;
 }
 </style>
