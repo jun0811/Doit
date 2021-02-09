c17a604cf110809e68de86f1f8893f4d5e4afe36
김부희 <kimbuhui@naver.com>
2021-02-10 00:20:24 +0900

    Feat: 마일리지 점수 지급하기

--- backend/src/main/java/com/ssafy/doit/service/GroupUserService.java
+++ backend/src/main/java/com/ssafy/doit/service/GroupUserService.java
@@ -1,15 +1,13 @@
 package com.ssafy.doit.service;
 
-import com.ssafy.doit.model.Group;
-import com.ssafy.doit.model.GroupHashTag;
-import com.ssafy.doit.model.GroupUser;
-import com.ssafy.doit.model.HashTag;
+import com.ssafy.doit.model.*;
 import com.ssafy.doit.model.response.ResGroupList;
 import com.ssafy.doit.model.response.ResponseBasic;
 import com.ssafy.doit.model.user.User;
 import com.ssafy.doit.model.user.UserRole;
 import com.ssafy.doit.repository.GroupRepository;
 import com.ssafy.doit.repository.GroupUserRepository;
+import com.ssafy.doit.repository.MileageRepository;
 import com.ssafy.doit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -34,6 +33,8 @@ public class GroupUserService {
     private UserRepository userRepository;
     @Autowired
     private GroupRepository groupRepository;
+    @Autowired
+    private MileageRepository mileageRepository;
 
     // 가입 그룹 리스트
     @Transactional
@@ -61,7 +62,12 @@ public class GroupUserService {
                     .group(group).user(user).build());
             group.setTotalNum(group.getTotalNum() + 1);
             groupRepository.save(group);
-            // 마일리지 증가 추가하기 예시 : user.setMileage(user.getMileage() + 30);
+            user.setMileage(user.getMileage() + 100);
+            userRepository.save(user);
+            mileageRepository.save(Mileage.builder()
+                    .content("그룹가입 축하 마일리지 지급")
+                    .date(LocalDate.now())
+                    .user(user).build());
         }else throw new Exception("이미 가입된 그륩입니다.");
     }
 
@@ -75,9 +81,14 @@ public class GroupUserService {
         Optional<GroupUser> opt = groupUserRepository.findByGroupAndUser(group, user);
         if(opt.isPresent()) {
             opt.ifPresent(selectGU -> groupUserRepository.delete(selectGU));
-            // 마일리지 감소 추가하기 예시 :  user.setMileage(user.getMileage() - 10);
             group.setTotalNum(group.getTotalNum() - 1); //회원 수 감소
             groupRepository.save(group);
+            user.setMileage(user.getMileage() - 1500);
+            userRepository.save(user);
+            mileageRepository.save(Mileage.builder()
+                    .content("그룹탈퇴 마일리지 차감")
+                    .date(LocalDate.now())
+                    .user(user).build());
         }else throw new Exception("가입되어 있지 않은 그룹원입니다.");
     }
 
@@ -87,16 +98,20 @@ public class GroupUserService {
         Group group = groupRepository.findById(groupPk).get();
         if(leader == group.getLeader()){
             User user = userRepository.findById(userPk).get(); // 강퇴시킬 그룹원
-            Optional<GroupUser> groupUser = groupUserRepository.findByGroupAndUser(group,user);
-            groupUser.ifPresent(selectUser ->{
-               groupUserRepository.delete(selectUser);
-            });
+            Optional<GroupUser> opt = groupUserRepository.findByGroupAndUser(group, user);
+            opt.ifPresent(selectGU -> groupUserRepository.delete(selectGU));
             group.setTotalNum(group.getTotalNum() - 1); // 회원 수 감소
             groupRepository.save(group);
+            user.setMileage(user.getMileage() - 1000);
+            userRepository.save(user);
+            mileageRepository.save(Mileage.builder()
+                    .content("그룹강퇴 마일리지 차감")
+                    .date(LocalDate.now())
+                    .user(user).build());
         }else throw new Exception("그룹장이 아닙니다.");
     }
 
-    // 랄퇴한 회원, 가입된 그룹에서 delete
+    // 회원 랄퇴(강퇴), 가입된 모든 그룹에서 delete
     @Transactional
     public void deleteGroupByUser(Long userPk) throws Exception {
         User user = userRepository.findById(userPk).get();
