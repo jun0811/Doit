d75189fb910e8095e360144edcfa1c96fe0b1ff4
최예은 <skyyy6@naver.com>
2021-02-11 01:17:59 +0900

    Fix: 상품 거래 마일리지 내역 추가

--- backend/src/main/java/com/ssafy/doit/model/response/ResponseProduct.java
+++ backend/src/main/java/com/ssafy/doit/model/response/ResponseProduct.java
@@ -2,12 +2,15 @@ package com.ssafy.doit.model.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import java.time.LocalDateTime;
+
 public interface ResponseProduct {
     Long getId();
     String getCategory();
     String getTitle();
     String getContent();
     String getImage();
+    LocalDateTime getCreateDate();
     int getMileage();
     boolean getStatus();
 
