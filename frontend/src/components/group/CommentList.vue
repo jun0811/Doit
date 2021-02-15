b836a471d1b3a2e1be27f20d76fee9739511ed52
dryans <dryans@naver.com>
2021-02-15 15:16:47 +0900

    Feat : 댓글 생성, 리스트 레이아웃

---
+++ frontend/src/components/group/CommentList.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <v-list>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://images.unsplash.com/photo-1578452496128-ab2d0703a8de?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title >아이디를 여기에</v-list-item-title>
+          <v-list-item-subtitle> 인풋창을 여기에 </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-list three-line>
+      <template v-for="(item, index) in items">
+        <v-subheader
+          v-if="item.header"
+          :key="item.header"
+          v-text="item.header"
+        ></v-subheader>
+
+        <v-divider
+          v-else-if="item.divider"
+          :key="index"
+          :inset="item.inset"
+        ></v-divider>
+
+        <v-list-item
+          v-else
+          :key="item.title"
+        >
+          <v-list-item-avatar>
+            <v-img :src="item.avatar"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </div>
+</template>
+
+<script>
+
+import http from "../../http-common";
+
+
+export default {
+  data: () => ({
+    feedPk : 44,
+    items: [
+        { header: '댓글목록' },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Brunch this weekend?',
+          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Oui oui',
+          subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          title: 'Birthday gift',
+          subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          title: 'Recipe to try',
+          subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        },
+      ],
+  }),
+  props : {
+
+  },
+  methods: {
+    getComment() {
+      http.get(`comment/commentList?feedPk=${this.feedPk}`)
+        .then((res)=>{
+          if(res.data.status){
+            console.log(res.data.object)
+          }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
--- frontend/src/components/group/FeedCard.vue
+++ frontend/src/components/group/FeedCard.vue
@@ -61,21 +61,18 @@
           <v-btn outlined class="my-3 btn green-btn" @click="feedUpdate">피드 수정 </v-btn>
           <v-btn outlined class="my-3 btn red-btn" @click="feedDelete">피드 삭제 </v-btn>    
         </div>
-        <!-- <v-card-actions> -->
-          <v-btn
-          icon
-          @click="show=!show">
-            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-          </v-btn>
-        <!-- </v-card-actions> -->
+        <v-btn
+        icon
+        @click="show=!show">
+          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
       </v-row>
     </v-card-text>
      <v-expand-transition>
       <div v-show="show">
         <v-divider></v-divider>
-
         <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          <Comment></Comment>
         </v-card-text>
       </div>
     </v-expand-transition>
@@ -84,11 +81,15 @@
 </template>
 
 <script>
+import Comment from '../../components/group/CommentList.vue'
 import http from '../../http-common'
 
   export default {
     props: {
       card: Object
+    },
+    components: {
+      Comment
     },
     data: () => ({
       loading: false,
