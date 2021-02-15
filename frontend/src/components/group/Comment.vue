69c72063ed2df4547deef5bd6f46d24d3232dc06
dryans <dryans@naver.com>
2021-02-15 18:59:27 +0900

    Feat : 댓글 목록 불러오기 api 연결 및 레이아웃

--- frontend/src/components/group/Comment.vue
+++ frontend/src/components/group/Comment.vue
@@ -31,30 +31,30 @@
       </v-row>
     </v-list>
     <v-list three-line>
-      <template v-for="(item, index) in items">
+      <template v-for="(comment, index) in comments">
         <v-subheader
-          v-if="item.header"
-          :key="item.header"
-          v-text="item.header"
+          v-if="comment.header"
+          :key="comment.header"
+          v-text="comment.header"
         ></v-subheader>
 
         <v-divider
-          v-else-if="item.divider"
+          v-else-if="comment.divider"
           :key="index"
-          :inset="item.inset"
+          :inset="comment.inset"
         ></v-divider>
 
         <v-list-item
           v-else
-          :key="item.title"
+          :key="comment.writerName"
         >
           <v-list-item-avatar>
-            <v-img :src="item.avatar"></v-img>
+            <v-img :src="comment.profileImg"></v-img>
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title v-html="item.title"></v-list-item-title>
-            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+            <v-list-item-title v-html="comment.writerName"></v-list-item-title>
+            <v-list-item-subtitle v-html="comment.content"></v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </template>
@@ -70,43 +70,14 @@ import http from "../../http-common";
 export default {
   data: () => ({
     feedPk : 44,
-    items: [
-        { header: '댓글목록' },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          title: 'Brunch this weekend?',
-          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
-          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-          title: 'Oui oui',
-          subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
-          title: 'Birthday gift',
-          subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
-        },
-        { divider: true, inset: true },
-        {
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
-          title: 'Recipe to try',
-          subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
-        },
-      ],
+    baseImg : 'https://ssafydoit.s3.ap-northeast-2.amazonaws.com/',
     comment : {
-      content:'',
-      feedPk: 44,
-      userPk: 8, 
-    },
+      content : '',
+      feedPk : 44,
+      userPk : 8
+    }, //작성자의 코멘트
+
+    comments :[],
     user :{}, //nickname, userPk, image가 들어있음
     btnActive:false,
   }),
@@ -117,7 +88,6 @@ export default {
     this.user = this.$store.getters.getName
     this.getComment()
     this.getUser()
-    // console.log(this.card)
   }, 
   methods: {
     createComment() {
@@ -126,29 +96,55 @@ export default {
         "feedPk" : this.comment.feedPk,
         "userPk" : this.user.userPk
       }
+      this.comment.content =''
       http.post(`comment/createComment`, params)
         .then((res)=>{
           if(res.data.status){
-            console.log(res.data.object)
+            console.logt(res.data.object)
           }
         }).catch((err) => {
           console.log(err)
         })
     }, 
-    getUser() {
-      http.get(`user/detailUser`)
-        .then((res)=>{
-          if(res.data.status){
-            this.user = res.data.object
-          }
-      })
-    },
     getComment() {
       http.get(`comment/commentList?feedPk=${this.card.feedPk}`)
         .then((res)=>{
           if(res.data.status){
             console.log('getComment', res.data.object)
-            this.comment.content =''
+            const response = res.data.object
+            this.comments = []
+            let cnt = 0;
+            this.comments = response.flatMap(comment=> {
+              if (cnt === 0) {
+                cnt = cnt + 1
+                return [
+                  { "header": '댓글목록' },
+                  {
+                  "content": comment.content,
+                  "profileImg" : this.baseImg + comment.image,
+                  "writerName": '유저이름',
+                  }
+                ]
+              } else {
+                return [
+                  { divider: true, inset: true },
+                  {
+                  "content": comment.content,
+                  "profileImg" : this.baseImg + comment.image,
+                  "writerName": '유저이름',
+                  }
+                ]
+              }
+            })
+            console.log(this.comments)
+          }
+      })
+    },
+    getUser() {
+      http.get(`user/detailUser`)
+        .then((res)=>{
+          if(res.data.status){
+            this.user = res.data.object
           }
       })
     },
