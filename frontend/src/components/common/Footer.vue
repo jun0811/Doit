2f8ac8f6966ad8ee480a34a25410cce6c0912e4d
dryans <dryans@naver.com>
2021-02-16 16:34:15 +0900

    Feat : footer 레이아웃

--- frontend/src/components/common/Footer.vue
+++ frontend/src/components/common/Footer.vue
@@ -1,45 +1,56 @@
 <template>
-    <v-footer
-      color="white lighten-1"
-      flat
-      padless
-      class="footer"
-    >
+  <div>
+    <v-footer 
+    color="#F9802D"
+    class="foot d-flex flex-column justify-center py-6 white--text">
       <v-row
         justify="center"
+
         no-gutters
       >
-        <v-btn
-          v-for="link in links"
-          :key="link"
-          color="black"
-          text
-          rounded
-          class="my-2"
-        >
-          {{ link }}
-        </v-btn>
+        <span class="mr-3">만든사람들 : </span>
+        <span v-for="(member,idx) in members" :key="idx" class="d-flex align-center">
+          <font-awesome-icon v-if="idx !== 0" class="circle mx-2" icon="circle"/>
+          <span >{{member}}</span>
+        </span>
+      </v-row >
+      <v-row no-gutters>
+        <span class="mr-3">소속 : </span>
+        <span>삼성청년소프트웨어(SSAFY) 4기 광주 1반</span>
+      </v-row>
+      <v-row no-gutters>
         <v-col
-          class="white lighten-2 py-4 text-center black--text"
+          class="py-4 text-center "
           cols="12"
         >
           {{ new Date().getFullYear() }} — <strong>Doit</strong>
         </v-col>
       </v-row>
     </v-footer>
+
+  </div>
+
 </template>
 
 <script>
   export default {
     data: () => ({
-      links: [
-        'Home',
-        'About Us',
-        'Team',
-        'Services',
-        'Blog',
-        'Contact Us',
-      ],
+      members:['김부희', '김소정','박다솔','이승준','최예은','한지현'],
     }),
   }
 </script>
+
+<style scoped>
+
+.foot {
+  display:absolute;
+  bottom:-100px;
+  background-color: darkorange;
+  color:lightgoldenrodyellow;
+}
+
+.circle{
+  width: 0.25rem;
+  height: 0.25rem;
+}
+</style>
