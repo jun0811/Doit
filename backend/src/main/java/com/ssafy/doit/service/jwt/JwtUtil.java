e3b235c00ee5f30f54e1cf5bbf4c724e5b4bff50
최예은 <skyyy6@naver.com>
2021-02-17 00:17:19 +0900

    Fix: 로그인 유지시간 변경

--- backend/src/main/java/com/ssafy/doit/service/jwt/JwtUtil.java
+++ backend/src/main/java/com/ssafy/doit/service/jwt/JwtUtil.java
@@ -21,7 +21,7 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    public final static long TOKEN_VALIDATION_SECOND = 1000L * 60 * 30;
+    public final static long TOKEN_VALIDATION_SECOND = 1000L * 60 * 60 * 24;
 
     final static public String ACCESS_TOKEN_NAME = "accessToken";
 
