8ee338e7aa29fb305ab4de21179e1b017d45412d
김소정 <itoggi0328@naver.com>
2021-02-17 23:34:26 +0900

    fix: 마일리지샵에서 물품 카드 넓이 고정

--- frontend/src/components/mileage/ProductPage.vue
+++ frontend/src/components/mileage/ProductPage.vue
@@ -12,10 +12,12 @@
         class="d-flex justify-center pa-4"
       >
           <v-card 
-          router-link :to="{name: 'ProductDetail', params: {product_id: product.id}}">
+            router-link :to="{name: 'ProductDetail', params: {product_id: product.id}}"
+            height="100%" width="100%"
+          >
             <v-img
               :src="`http://ssafydoit.s3.ap-northeast-2.amazonaws.com/` + product.image"
-              class="white--text align-end product-image"
+              class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
               height="200px"
             >
