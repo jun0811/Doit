85b4a2923e8d41478ff8625fe5ec9f753a88b6b0
김부희 <kimbuhui@naver.com>
2021-02-17 22:16:13 +0900

    Modify: 그룹리스트 수정

--- backend/src/main/java/com/ssafy/doit/repository/group/GroupRepository.java
+++ backend/src/main/java/com/ssafy/doit/repository/group/GroupRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.doit.repository.group;
 import com.ssafy.doit.model.group.Group;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,5 +15,7 @@ public interface GroupRepository extends JpaRepository<Group, Long> {
             "where h.group = g and h.hashTag.name like %:hashtag% ) and g.status = :status")
     Page<Group> findAllByHashTagAndStatus(@Param("hashtag") String hashtag, String status, Pageable pageable);
     Page<Group> findAllByCategoryAndStatus(String category, String status, Pageable pageable);
+    Page<Group> findAllByStatus(String status, Pageable pageable);
     Optional<Group> findByGroupPkAndStatus(Long groupPk, String status);
+
 }
