6d5aead14c695bf31ffe362cfd48505b80f74c79
이승준 <nate199458@gmail.com>
2021-02-18 19:48:39 +0900

    feat: 관리페이지 테이블 - 테이블 구현 - 강퇴구현

---
+++ frontend/src/components/admin/GroupList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <vue-good-table
+      :columns="columns"
+      max-height="500px"
+      :fixed-header="true"
+      :rows="rows"/>
+  </div>
+</template>
+
+<script>
+import http from "../../http-common"
+import { VueGoodTable } from 'vue-good-table';
+import 'vue-good-table/dist/vue-good-table.css'
+
+export default {
+  components: {
+    VueGoodTable
+  },
+  data(){
+    return{
+      columns:[
+        {
+          label: '그룹번호',
+          field: 'groupPk',
+          type: 'number'
+        },
+        {
+          label: '카테고리',
+          field: 'category',
+        },
+        {
+          label: '그룹명',
+          field: 'name',
+        },
+        {
+          label: '팀스코어',
+          field: 'score',
+          type: 'number'
+        },
+        {
+          label: '총인원수',
+          field: 'totalNum',
+          type: 'number'
+        },
+        ],
+      rows: []
+    }
+  },
+  created(){
+    http.get("/admin/searchAllGroup")
+    .then((res)=>{
+      this.rows = res.data.object
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Admin.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>관리자 페이지</h3>
+    <v-container>
+      <v-row>
+        <v-col>
+          <GroupList></GroupList>
+        </v-col>
+      </v-row>
+      <v-row class="mt-10">
+        <v-col>
+          <MemberList></MemberList>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import http from "../http-common"
+import GroupList from "@/components/admin/GroupList"
+import MemberList from "@/components/admin/MemberList"
+export default {
+  components:{
+    GroupList, MemberList
+  },
+  created () {
+    http.get('user/detailUser')
+    .then((res)=>{
+      console.log(res)
+    })
+
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
